941ff48a40f52cd7cf190d5bcbd6bd3c2ab2feb3
Joran Angevaare <jorana@nikhef.nl>
2020-11-23 23:02:09 +0100

    Add matching tools

---
+++ peak_matching/__init__.py
@@ -0,0 +1,6 @@
+from .match_peaks import *
+from .recarray_tools import *
+from .wfsim_utils import *
+from .match_plugins import *
+from .summary_plots import *
+
---
+++ tests/test_basics.py
@@ -0,0 +1,6 @@
+import peak_matching
+
+from peak_matching import match_peaks
+from peak_matching import MatchPeaks
+
+print('done')
