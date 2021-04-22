d3329f1a156d9b782eb46609cebb07808917826a
Joran Angevaare <jorana@nikhef.nl>
2021-04-22 13:27:25 +0200

    Update notebooks to be more plug and play (#13)
    * get ready for 0.2.0
    
    * write explanation on notebook

---
+++ notebooks/setup_highe.py
@@ -0,0 +1,60 @@
+import pema
+import os
+import straxen
+
+base_dir = '/dali/lgrandi/angevaare/wfsims/pema'
+data_name = f'pema_highe_{pema.__version__}'
+fig_dir = os.path.join(base_dir, f'figures_summary_{data_name}_update')
+data_dir = os.path.join(base_dir, 'processed_data')
+raw_data_dir = os.path.join(base_dir, 'raw_data')
+instructions_csv = f"./inst_{data_name}.csv"
+
+# Output naming
+default_label = 'Normal clustering'
+custom_label = 'Changed clustering'
+# You need this for setting up the dali-jobs
+environ_init = '''eval "$(/home/angevaare/software/Miniconda3/bin/conda shell.bash hook)"
+conda activate strax
+export PATH=/home/angevaare/software/Miniconda3/envs/strax/bin:$PATH'''
+
+
+# Output naming
+default_label = 'Normal clustering'
+custom_label = 'Changed clustering'
+
+# Take a few arbitrary runs that allow to run jobs in parallel and get the 
+# gains from CMT
+run_list = list(f'{r:06}' for r in range(12000,12000+40))
+
+# Just some id which allows CMT to load
+run_id = run_list[0]
+
+# setting up instructions like this may take a while. You can set e.g. 
+instructions = dict(
+    event_rate=100,
+#     event_rate=400, # Don't make too large -> overlapping truth info
+    chunk_size=5, # keep large -> less overhead but takes more RAM
+    nchunk=1, # set to 100
+    photons_low=1, #PE
+    photons_high=1e5, #PE
+    electrons_low=1, #
+    electrons_high=1e4,
+    tpc_radius=straxen.tpc_r,
+    tpc_length=straxen.tpc_z,
+    drift_field = straxen.get_resource('fax_config_nt_low_field.json', fmt='json').get('drift_field'),
+    timing = 'uniform', #Double S1 peaks uniform over time   
+)
+
+pema.inst_to_csv(
+    instructions, 
+    instructions_csv, 
+    get_inst_from = pema.rand_instructions)
+
+
+# TODO can we add noise?
+config_update = dict(
+    detector='XENONnT',
+    fax_file=os.path.abspath(instructions_csv),
+    fax_config='fax_config_nt_low_field.json',
+)
+
