1b2b0d91de0f803c8e8979caadce4cf83927774c
jorana <jorana@nikhef.nl>
2021-05-21 14:41:36 +0200

    fix codefactor some more

--- tests/test_matching.py
+++ tests/test_matching.py
@@ -139,7 +139,7 @@ def test_deepwindows(data_length,
     windows = strax.touching_windows(allpeaks1, allpeaks2, window=matching_fuzz)
     deepwindows_simple = get_deepwindows(windows, allpeaks1, allpeaks2, matching_fuzz)
     deepwindows_numba = pema.matching.get_deepwindows(windows, allpeaks1, allpeaks2, matching_fuzz)
-    if len(deepwindows_simple) and not len(deepwindows_numba):
+    if len(deepwindows_simple) >= 0 and not len(deepwindows_numba):
         assert np.all(deepwindows_simple == deepwindows_numba)
 
 
@@ -152,7 +152,7 @@ def get_deepwindows(windows, peaks_a, peaks_b, matching_fuzz):
             match = strax.touching_windows(peaks_a,
                                            peaks_b[l1:r1],
                                            window=matching_fuzz)
-            if len(match) >= 1:
+            if len(match) >= 0:
                 this_window = match[0]
             else:
                 pass
