ca6beb4bca8c0e065e10273e7c3f6b08e6fbc2ef
Joran Angevaare <jorana@nikhef.nl>
2022-02-09 12:27:31 +0100

    further updates

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,9 +1,10 @@
-from unittest import TestCase
+from unittest import TestCase, skipIf
 import numpy as np
 import pema
+import straxen
+
 
 def test_import():
-
     from pema import matching
     from pema import MatchPeaks
     from pema import append_fields
@@ -19,6 +20,7 @@ def test_import():
 
 class SimpleTests(TestCase):
     """Odd bunch of tests that can be removed if needed"""
+
     def check_raises_check_args(self):
         truth = np.zeros(1, dtype=[(('bla bla', 'bla'), np.int8)])
         with self.assertRaises(ValueError):
@@ -27,10 +29,22 @@ class SimpleTests(TestCase):
             pema.compare_plots._check_args([], truth_vs_custom=truth, run_id='bla')
 
     def test_show(self):
-        pema.compare_plots('a', 'b', show=True)
+        pema.compare_plots._save_and_show('a', 'b', show=True, peak_i=1)
 
     def test_warnings_for_matching(self):
         with self.assertRaises(ValueError):
             peak1 = np.zeros(1, dtype=[(('bla bla', 'bla'), np.int8)])
             peak2 = peak1
             pema.match_peaks(peak1, peak2)
+
+    @skipIf(straxen.utilix_is_configured(), 'no utilix file')
+    def test_context(self):
+        with self.assertRaises(FileNotFoundError):
+            pema.pema_context(base_dir='/the/moon/path', fax_config='bla', cmt_run_id_sim='1')
+        with self.assertRaises(ValueError):
+            pema.pema_context(base_dir='./', config_update=1, fax_config='bla', cmt_run_id_sim='1')
+        with self.assertRaises(RuntimeError):
+            pema.pema_context(base_dir='./',
+                              config_update=1,
+                              fax_config='fax_config_nt_design.json',
+                              cmt_run_id_sim='1')
